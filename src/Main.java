480378fdf63eaaf7c2159b95da17540b49b7e520
liangb.dev <liangb.dev@gmail.com>
2018-06-19 23:11:14 +0200

    complete

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,9 @@ public class Main {
 
         // Testing algorithm DiffTwoArrays
         IntermediateAlgorithm object = new BinaryAgents("01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001");
-        object.method(1);
+        //object.method(1);
+
+
 
     }
 }
